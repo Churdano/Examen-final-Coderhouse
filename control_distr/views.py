f76b429599e53670a30b9fb162551d04ada921b5
Gonzalo <gonzalink88@gmail.com>
2023-05-07 14:01:02 -0300

    agregados los templates

--- control_distr/views.py
+++ control_distr/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def saludar_con_html(request):
+    contexto = {}
+    HttpResponse = render(
+    request = request,
+    template_name = 'control_distr/base.html',
+    context = contexto,
+    )
+    
+    return HttpResponse
+    
--- distribuidora/urls.py
+++ distribuidora/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from control_distr.views import saludar_con_html
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo-html/', saludar_con_html),
+    
+    
 ]
