a9cfefec295264b70b08cdde88f7f564c1023318
Gonzalo <gonzalink88@gmail.com>
2023-05-08 21:34:13 -0300

    Agregados los errores para campos de formmulario

---
+++ control_distr/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ClienteForm(forms.Form):
+    nombre = forms.CharField(max_length=100)
+    apellido = forms.CharField(max_length=100)
+    numero_cliente = forms.IntegerField()
+    calle = forms.CharField(max_length=100)
+    calle_altura = forms.IntegerField()
+    telefono = forms.IntegerField()
--- control_distr/views.py
+++ control_distr/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from control_distr.models import productos, clientes
+from .forms import ClienteForm
+
 
 def saludar_con_html(request):
     contexto = {
@@ -20,19 +22,26 @@ def listar_clientes(request):
     }
     return render(request, 'control_distr/lista_clientes.html', contexto)
 
+
 def crear_cliente(request):
     if request.method == "POST":
-        data = request.POST
-        nombre = data.get("nombre")
-        apellido = data.get("apellido")
-        numero_cliente = data.get("numero_cliente")
-        calle = data.get("calle")  
-        calle_altura = data.get("calle_altura")
-        telefono = data.get("telefono")  
-        cliente = clientes(nombre=nombre, apellido=apellido, numero_cliente=numero_cliente, calle=calle, calle_altura=calle_altura, telefono=telefono)
-        cliente.save()
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data["nombre"]
+            apellido = form.cleaned_data["apellido"]
+            numero_cliente = form.cleaned_data["numero_cliente"]
+            calle = form.cleaned_data["calle"]
+            calle_altura = form.cleaned_data["calle_altura"]
+            telefono = form.cleaned_data["telefono"]
+            cliente = clientes(nombre=nombre, apellido=apellido, numero_cliente=numero_cliente, calle=calle, calle_altura=calle_altura, telefono=telefono)
+            cliente.save()
+            url_exitosa = reverse('lista_clientes')
+            return redirect(url_exitosa)
+    else:
+        form = ClienteForm()
+    
+    contexto = {
+        'form': form
+    }
+    return render(request, 'control_distr/formulario_cliente.html', contexto)
 
-        url_exitosa = reverse('lista_clientes')
-        return redirect(url_exitosa)
-    else: 
-        return render(request, 'control_distr/formulario_cliente.html')
