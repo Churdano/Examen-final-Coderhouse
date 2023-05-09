eb2e85b55a22c698fc8e31702a4e17ee4092147f
Gonzalo <gonzalink88@gmail.com>
2023-05-08 21:19:07 -0300

    agregados los cambios

--- control_distr/urls.py
+++ control_distr/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from control_distr.views import listar_productos, listar_clientes, crear_cliente
 
--- control_distr/views.py
+++ control_distr/views.py
@@ -1,49 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from control_distr.models import productos, clientes
 
 def saludar_con_html(request):
     contexto = {
         'saludo': 'Bienvenido a distribuidora LG!',
     }
-    HttpResponse = render(
-        request=request,
-        template_name='control_distr/inicio.html',
-        context=contexto,
-    )
-    
-    return HttpResponse
+    return render(request, 'control_distr/inicio.html', contexto)
 
 def listar_productos(request):
     contexto = {
         "productos": productos.objects.all(),
     }
-    HttpResponse = render(
-        request=request,
-        template_name='control_distr/lista_productos.html',
-        context=contexto,
-    )
-    
-    return HttpResponse
-    
-    
+    return render(request, 'control_distr/lista_productos.html', contexto)
+
 def listar_clientes(request):
     contexto = {
         "clientes": clientes.objects.all(),
     }
-    HttpResponse = render(
-        request=request,
-        template_name='control_distr/lista_clientes.html',
-        context=contexto,
-    )
-    
-    return HttpResponse
+    return render(request, 'control_distr/lista_clientes.html', contexto)
 
 def crear_cliente(request):
-    contexto = {}
-    HttpResponse = render(
-        request=request,
-        template_name='control_distr/formulario_cliente.html',
-        context=contexto,
-    )
-    
-    return HttpResponse
+    if request.method == "POST":
+        data = request.POST
+        nombre = data.get("nombre")
+        apellido = data.get("apellido")
+        numero_cliente = data.get("numero_cliente")
+        calle = data.get("calle")  
+        calle_altura = data.get("calle_altura")
+        telefono = data.get("telefono")  
+        cliente = clientes(nombre=nombre, apellido=apellido, numero_cliente=numero_cliente, calle=calle, calle_altura=calle_altura, telefono=telefono)
+        cliente.save()
+
+        url_exitosa = reverse('lista_clientes')
+        return redirect(url_exitosa)
+    else: 
+        return render(request, 'control_distr/formulario_cliente.html')
